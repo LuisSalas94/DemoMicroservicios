0f563f2999552681764c71dabb977bea83a02e64
Fernando Salas <luisfernandosalasg@gmail.com>
2024-01-11 05:51:11 -0500

    seccion2/Entity Producto

---
+++ Seccion 2/Spring-Boot-Servicio-Productos/src/main/java/com/app/productos/models/entity/Producto.java
@@ -0,0 +1,29 @@
+package com.app.productos.models.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "productos")
+public class Producto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private Double precio;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    private static final long serialVersionUID = 1L;
+}
